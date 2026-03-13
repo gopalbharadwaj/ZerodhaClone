850df14ed9e2fd7d2f27fda418644476301784ee
Gopal Bhardwaj <gopalbhardwaj2627@gmail.com>
2026-03-14 00:46:57 +0530

    fix port for render

--- backend/index.js
+++ backend/index.js
@@ -59,6 +59,12 @@ res.send("Order saved!");
 
 });
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000");
+// app.listen(5000,()=>{
+// console.log("Server running on port 5000");
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
