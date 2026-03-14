2562996c65ae1cd016b9ac7c1d53cfd9191c13e0
Gopal Bhardwaj <gopalbhardwaj2627@gmail.com>
2026-03-14 12:46:26 +0530

    fix signup

--- frontend/src/landing_page/signup/SignupForm.js
+++ frontend/src/landing_page/signup/SignupForm.js
@@ -178,7 +178,7 @@ const [password,setPassword] = useState("")
 const handleSignup = async(e)=>{
 e.preventDefault()
 
-const res = await fetch("http://localhost:5000/api/auth/signup",{
+const res = await fetch("https://zerodhaclone-e995.onrender.com/api/auth/signup",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
