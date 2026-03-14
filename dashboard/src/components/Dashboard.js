aa47b72a01bbae2f819fa2c821af94963fea12f4
Gopal Bhardwaj <gopalbhardwaj2627@gmail.com>
2026-03-14 23:40:47 +0530

    fix dashboard project

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,37 +1,86 @@
+// import React, { useEffect } from "react";
+// import { Route, Routes } from "react-router-dom";
+
+// import Apps from "./Apps";
+// import Funds from "./Funds";
+// import Holdings from "./Holdings";
+
+// import Orders from "./Orders";
+// import Positions from "./Positions";
+// import Summary from "./Summary";
+// import WatchList from "./WatchList";
+// import { GeneralContextProvider } from "./GeneralContext";
+
+
+
+// const Dashboard = () => {
+//   useEffect(() => {
+
+//     const token = localStorage.getItem("token");
+
+//     if (!token) {
+//       window.location.href = "/login";
+//     }
+
+//   }, []);
+//   return (
+//     <div className="dashboard-container">
+
+//       <GeneralContextProvider>
+//         <WatchList />
+//       </GeneralContextProvider>
+//       <div className="content">
+//         <Routes>
+//           <Route exact path="/" element={<Summary />} />
+//           <Route path="/orders" element={<Orders />} />
+//           <Route path="/holdings" element={<Holdings />} />
+//           <Route path="/positions" element={<Positions />} />
+//           <Route path="/funds" element={<Funds />} />
+//           <Route path="/apps" element={<Apps />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 import Apps from "./Apps";
 import Funds from "./Funds";
 import Holdings from "./Holdings";
-
 import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
 import { GeneralContextProvider } from "./GeneralContext";
 
-
-
 const Dashboard = () => {
+
+  const navigate = useNavigate();
+
   useEffect(() => {
 
     const token = localStorage.getItem("token");
 
     if (!token) {
-      window.location.href = "/login";
+      navigate("/login");
     }
 
-  }, []);
+  }, [navigate]);
+
   return (
     <div className="dashboard-container">
 
       <GeneralContextProvider>
         <WatchList />
       </GeneralContextProvider>
+
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Summary />} />
+          <Route path="/" element={<Summary />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
@@ -39,6 +88,7 @@ const Dashboard = () => {
           <Route path="/apps" element={<Apps />} />
         </Routes>
       </div>
+
     </div>
   );
 };
